ebefd011ee9fd51baf66def62f4b470323b17c5a
107590023 <t107590023@ntut.org.tw>
2021-03-24 15:04:24 +0800

    Add card game function

--- cardGame/cardGame/MatchingGame.swift
+++ cardGame/cardGame/MatchingGame.swift
@@ -9,17 +9,55 @@ import Foundation
 
 class MatchingGame{
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
     var cards: Array<Card> = Array()
+    var current:Bool = false
     //var cards: [Card] = [Card]()
     //var cards: [Card] = []
     //var cards: = [Card]()
     func chooseCard(at index: Int){
-        if cards[index].isFaceUp{
-            cards[index].isFaceUp = false
-        }else{
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched{
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
+                if cards[matchIndex].Identifier == cards[index].Identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            }else if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex == index{
+                cards[index].isFaceUp = false
+                indexOfOneAndOnlyFaceUpCard = nil
+            }else{
+                for flipDownIndex in cards.indices{
+                    if !cards[flipDownIndex].isMatched {
+                        cards[flipDownIndex].isFaceUp = false
+                        
+                    }
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
+    
+    func flipAllCard(){
+        for ind in cards.indices{
+            if(current && cards[ind].isFaceUp){ cards[ind].isFaceUp = !cards[ind].isFaceUp
+            }else if (!current && !cards[ind].isFaceUp){
+                cards[ind].isFaceUp = !cards[ind].isFaceUp
+            }
+        }
+        current = !current;
+
+    }
+    func resetAllCard(){
+        cards.shuffle()
+        for ind in cards.indices{
+            cards[ind].isFaceUp = false
+            cards[ind].isMatched = false
+        }
+    }
+
     init(numberOfPairsOfCards: Int){
         //if 沒有自定義 init
         //let card = Card(isFaceUp: false, isMatched: false, Identifier: 0)
@@ -36,9 +74,13 @@ class MatchingGame{
             //cards.appen(card) can simplfied as cards += [card, card]
             //In order to avoid setting id everytime change the init func of card, create a static variable and hava a new getID func. Also, we dont neet i in the for loop, so make it to a underline
             let card = Card()
+            //cards.append(card)
             cards += [card,card]
+        cards.shuffle()
             
         }
         
     }
+    
+    
 }
--- cardGame/cardGame/ViewController.swift
+++ cardGame/cardGame/ViewController.swift
@@ -9,11 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
     var display:String = "FLIPs: "
     var current:Bool = false
-
     @IBOutlet var cardButtons: [UIButton]!
     
     @IBOutlet weak var flipCount: UILabel!
@@ -22,7 +19,6 @@ class ViewController: UIViewController {
             flipCount.text = "Flips: \(count)"
         }
     }
-    
     //lazy: initialize when somebody use it. The trade-off will be we can not use "didset" in lazy variable
     lazy var game:MatchingGame = MatchingGame(numberOfPairsOfCards: (cardButtons.count+1)/2)
     
@@ -31,62 +27,56 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     var Emojis = ["🥵", "👾", "🤔", "🥱", "🥶", "🤯", "🤩", "😈"]
+    var emojiDic:Dictionary<Int,String> = Dictionary<Int,String>()
+    //emojiDic = Dictionary<Int,String>()
+    //emojiDic = [Int:String]()
+    
     @IBAction func touchCard(_ sender: UIButton) {
-
         count = count + 1
-        
-//        if let cardIndex = cardButtons.firstIndex(of: sender){
-//
-//            let emoji = Emojis[cardIndex]
-//            if(sender.currentTitle == emoji){
-//                sender.setTitle("",for: UIControl.State.normal)
-//                sender.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-//            }else{
-//                sender.setTitle(emoji,for: UIControl.State.normal)
-//                sender.backgroundColor = #colorLiteral(red: 0.6899590735, green: 1, blue: 0.7358576945, alpha: 1)
-//            }
-//            //sender.setTitle("",for: UIControl.State.normal)
-//        }
-        if let cardNumber = cardButtons.index(of: sender){
+        if let cardNumber = cardButtons.firstIndex(of: sender){
             print("cardNumber = \(String(describing: cardNumber))")
-            
             game.chooseCard(at: cardNumber)
             updateViewFromModel()
         }else{
             print("not in the collection")
         }
         
+        
     }
-    
     @IBAction func btn_Flip(_ sender: Any) {
+        game.flipAllCard()
+        updateViewFromModel()
         count = 0
-        for button in cardButtons{
-            if(current && button.currentTitle == ""){ button.sendActions(for: .touchUpInside)
-            }else if (!current && button.currentTitle != ""){
-                button.sendActions(for: .touchUpInside)
-            }
-        }
-        current = !current;
-        count = 0;
     }
-    
+    @IBAction func btn_reset(_ sender: Any) {
+        game.resetAllCard()
+        updateViewFromModel()
+        count = 0
+    }
     func updateViewFromModel(){
         for index in cardButtons.indices{
             let button = cardButtons[index]
             let card = game.cards[index]
             if card.isFaceUp{
-                button.setTitle(getEmoji(for: card), for: UIControl.State.normal)
-                button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0):#colorLiteral(red: 0.6899590735, green: 1, blue: 0.7358576945, alpha: 1)
             }else{
                 button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0):#colorLiteral(red: 0.6899590735, green: 1, blue: 0.7358576945, alpha: 1)
+                button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
             }
         }
     }
     
-    func getEmoji(for card: Card) -> String{
-        return "?"
-    }
+//    func getEmoji(for card: Card) -> String{
+//        return Emojis[card.Identifier-1]
+//    }
     
+    func emoji(for card: Card) -> String{
+        if emojiDic[card.Identifier] == nil, Emojis.count > 0{
+            let randomIndex = Int(arc4random_uniform(UInt32(Emojis.count)))
+            emojiDic[card.Identifier] = Emojis.remove(at: randomIndex)
+        }
+        return emojiDic[card.Identifier] ?? "?"
+    }
 
 }
